db0d1f1cf36b9e1ea5e58ea8b29b95814fb2c4a7
Artur Pelo <artur.pelo@gmail.com>
2024-10-24 10:15:44 +0200

    dodanie liczenia silni rekurencyjnie

--- MUP_R2IS_AlgorytmyIStrukturyDanych/Rekurencja.cpp
+++ MUP_R2IS_AlgorytmyIStrukturyDanych/Rekurencja.cpp
@@ -2,11 +2,17 @@
 
 long silnia(int n){
     int wynik=1;
-    for(i = 1; i < n; i++){
-
+    for(i = 1; i <= n; i++)
         wynik = wynik * i;
-    }
 return wynik;
+}
+
+long silniaRekurencyjnie(int n){
+
+    if(n>1)
+        return n*silniaRekurencyjnie(n-1);
+    else
+        return 1;
 
 }
 
@@ -18,7 +24,8 @@ int main(){
 // 1! =1
 // 5! = 1,2,3,4,5
 
-std::cout<<silnia(3);
+std::cout<<silnia(3)<<std::endln;
+std::cout<<silniaRekurencyjnie(3)<<std::endln;;
 
 
 return 0;
